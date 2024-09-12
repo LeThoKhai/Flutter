76c05b1c8cfc52677b93d836cb19c28f38a30e38
Le tho khai <trannguyenvu774411@gmail.com>
2024-09-12 17:01:38 +0700

    comit2

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Web của khải đập trai'),
+      home: const MyHomePage(title: 'Web của khải đập trai vcl'),
     );
   }
 }
